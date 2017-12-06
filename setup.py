70b5ef690eacd67b230f028de70fda7ad353b15b
Tim Hoyt <tim.c.hoyt@gmail.com>
2017-12-05 21:18:58 -0500

    Fixed setup.py. Seems to work okay

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 # Adapted from Xilnx Pynq pip-install setup.py script
 # This should probably be a make file with defconfig instead...
 
@@ -16,8 +18,7 @@ CPU_ARCH_IS_SUPPORTED = ZYNQ_ARCH in CPU_ARCH
 BOARD = os.environ['BOARD']
 
 # Deploy Path
-DEPLOY_DIR = BOARD + 'bsp/rootfs_part/home/ubuntu/pynq/'
-DEPLOY_LIB = BOARD + 'bsp/rootfs_part/home/ubuntu/pynq/lib/'
+DEPLOY_DIR = './boards/' + BOARD + '/bsp/rootfs_part/home/ubuntu/'
 
 # Pynq Path
 PYNQ_DIR = './pynq/'
@@ -37,33 +38,34 @@ pynq_lib_modules = \
 def run_make(src_path, dst_path, output_lib):
 	status = subprocess.check_call(["make", "-C", src_path])
 	if status is not 0:
-    	print("Error while running make for", output_lib, "Exiting..")
-        sys.exit(1)
-    else: shutil.copyfile(src_path + output_lib, dst_path + output_lib)
+		print("Error while running make for", output_lib, "Exiting..")
+		sys.exit(1)
+	else:
+		shutil.copyfile(src_path + output_lib, dst_path + output_lib)
 
 # Copy pynq files from src to dest
 def cp_pynq(src_path,dst_path):
 	for i in range(0,len(src_path)):
-		shutil.copyfile(src_path[i], dst_path + '.')
+		shutil.copyfile(src_path[i], dst_path + src_path[i])
 
 def main():
 	if BOARD is None:
-	    print("Please set the BOARD environment variable (e.g. Pynq-Z1).")
+		print("Please set the BOARD environment variable (e.g. Pynq-Z1).")
 		sys.exit(1)
 
-	# Build Pynq Libraries, copy to DEPLOY_LIB
+	# Build Pynq Libraries, copy to DEPLOY_DIR
 	if CPU_ARCH_IS_SUPPORTED:
-	   run_make("pynq/lib/_pynq/_apf/", DEPLOY_LIB, "libdma.so")
-	   run_make("pynq/lib/_pynq/_audio/", DEPLOY_LIB, "libaudio.so")
-	   run_make("pynq/libsds/xlnkutils/", DEPLOY_LIB, "libsds_lib.so")
-	elif(not CPU_ARCH_IS_SUPPORTED):
-	   warnings.warn("Pynq does not support the CPU Architecture: {}".format(CPU_ARCH), ResourceWarning)
+		run_make("pynq/lib/_pynq/_apf/", DEPLOY_DIR + 'pynq/lib/', "libdma.so")
+		run_make("pynq/lib/_pynq/_audio/", DEPLOY_DIR + 'pynq/lib/', "libaudio.so")
+		run_make("pynq/libsds/xlnkutils/", DEPLOY_DIR + 'pynq/lib/', "libsds_lib.so")
+	else:
+		warnings.warn("Pynq does not support the CPU Architecture: {}".format(CPU_ARCH), ResourceWarning)
 
 	# Copy Supported pynq modules to DEPLOY_DIR
-	cp_pynq([pynq_modules,DEPLOY_DIR])
+	cp_pynq(pynq_modules,DEPLOY_DIR)
 	
-	# Copy Supported pynq libs to DEPLOY_LIB
-	cp_pynq([pynq_modules,DEPLOY_LIB])
+	# Copy Supported pynq libs to DEPLOY_DIR
+	cp_pynq(pynq_lib_modules,DEPLOY_DIR)
 
 if __name__ == "__main__":
     main()
